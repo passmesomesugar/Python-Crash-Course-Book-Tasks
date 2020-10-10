8f3f478a874ba7c5f747e49774d5e52cf3e696c2
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-10-10 10:40:54 +0600

    3.8 some mistakes are removed

--- 3.8.py
+++ 3.8.py
@@ -7,59 +7,65 @@ Created on Fri Jul 24 16:18:49 2020
 
 # 3-8. Seeing the World: Think of at least five places in the world you’d like to
 # visit.
+
+
 # • Store the locations in a list. Make sure the list is not in alphabetical order.
+list = ["Akihabara", "Zanzibar" ,"New York", "Los Angeles","Neverland", "Nevermoreland", "Azeroth" ]
+
 # • Print your list in its original order. Don’t worry about printing the list neatly,
 # just print it as a raw Python list.
-# • Use sorted() to print your list in alphabetical order without modifying the
-# actual list.
-# • Show that your list is still in its original order by printing it.
-# • Use sorted() to print your list in reverse alphabetical order without changing
-# the order of the original list.
-# • Show that your list is still in its original order by printing it again.
-# • Use reverse() to change the order of your list. Print the list to show that its
-# order has changed.
-# • Use reverse() to change the order of your list again. Print the list to show
-# it’s back to its original order.
-# • Use sort() to change your list so it’s stored in alphabetical order. Print the
-# list to show that its order has been changed.
-# • Use sort() to change your list so it’s stored in reverse alphabetical order.
-# Print the list to show that its order has changed.
-
-
-list = ["Akihabara", "New York", "Los Angeles","Neverland", "Nevermoreland" ]
 print("Original order:")
 print(list)
 print("\n")
 
+# • Use sorted() to print your list in alphabetical order without modifying the
+# actual list.
 print("Sorted order:")
 print(sorted(list))
 print("\n")
 
+# • Show that your list is still in its original order by printing it.
 print("Original order:")
 print(list)
 print("\n")
 
-print(".reverse() order")
+# • Use sorted() to print your list in reverse alphabetical order without changing
+# the order of the original list.
+print("Reverse alphabetical order")
+print(sorted(list,reverse=True))
+print("\n")
+
+# • Show that your list is still in its original order by printing it again.
+print("Original order:")
+print(list)
+print("\n")
+
+# • Use reverse() to change the order of your list. Print the list to show that its
+# order has changed.
+print("reverse() used:")
 list.reverse()
 print(list)
 print("\n")
 
-print(".reverse() once again")
+# • Use reverse() to change the order of your list again. Print the list to show
+# it’s back to its original order.
+print("reverse() used again:")
 list.reverse()
 print(list)
 print("\n")
 
-print("sort() used:")
+# • Use sort() to change your list so it’s stored in alphabetical order. Print the
+# list to show that its order has been changed.
+print("sort() used, alphab. order now")
 list.sort()
 print(list)
 print("\n")
 
-print("sort(reverse=True) used again:")
+# • Use sort() to change your list so it’s stored in reverse alphabetical order.
+# Print the list to show that its order has changed.
+print("sort() with reverse")
 list.sort(reverse=True)
 print(list)
-print("\n")
-
-
 
 
 
