62160ddd3a904b59e08f1e77eab52f44ce44b338
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-10-10 10:02:15 +0600

    2.4 added

--- 2.4.py
+++ 2.4.py
@@ -9,4 +9,10 @@ Created on Wed Jul 22 17:51:55 2020
 # 2-4. Name Cases: Use a variable to represent a person’s name, and then print
 # that person’s name in lowercase, uppercase, and title case.
 
-
+name = "hu jintao"
+message = name.lower()
+print(message)
+message=name.upper()
+print(message)
+message= name.title() 
+print(message)
