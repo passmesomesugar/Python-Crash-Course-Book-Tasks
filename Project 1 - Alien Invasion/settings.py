1dd8b53d9884991b80a3fd507f2a4cba4c0bebe9
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-01-23 20:02:39 +0600

    minor changes in AI project

--- Project 1 - Alien Invasion/settings.py
+++ Project 1 - Alien Invasion/settings.py
@@ -1,8 +1,8 @@
 class Settings:
 
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (255, 250, 250)
 
         self.ship_speed = 1.5
