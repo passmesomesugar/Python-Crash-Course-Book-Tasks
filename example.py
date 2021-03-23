f4be082e40c5be84d87adae1e62065456f0e7513
Bagjan <sadvakasov.bagjan@gmail.com>
2021-03-23 13:51:47 +0600

    test

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import json
 
-numbers = [2, 3, 5, 7, 11, 13]
+numbers = [2, 3, 5, 7, 11, 14]
 filename = 'Do It Yourself section tasks/Chapter 10/numbers.json'
 with open(filename, 'w') as file_object:
     json.dump(numbers, file_object)
